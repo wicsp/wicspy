71be4908b6df1ad3b0a2b8827e761d06fd4747b0
wicsp <wicspa@gmail.com>
2024-06-05 16:54:11 +0800

    update package

--- build/lib/wicspy.py
+++ build/lib/wicspy.py
@@ -2,13 +2,13 @@
 Author: wicsp wicspa@gmail.com
 Date: 2024-06-05 13:44:37
 LastEditors: wicsp wicspa@gmail.com
-LastEditTime: 2024-06-05 16:05:37
+LastEditTime: 2024-06-05 16:41:47
 FilePath: /wicspy/wicspy.py
 Description: A Python package for wicsp.
 
 Copyright (c) 2024 by wicsp, Licensed under the MIT license.
 '''
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 from bark import *
