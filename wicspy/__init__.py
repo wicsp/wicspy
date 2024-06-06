0e75cc685b4c56fd753752fd3afe2d1529f12eca
wicsp <wicspa@gmail.com>
2024-06-06 16:22:06 +0800

    update

--- wicspy/__init__.py
+++ wicspy/__init__.py
@@ -2,7 +2,7 @@
 Author: wicsp wicspa@gmail.com
 Date: 2024-06-05 17:29:03
 LastEditors: wicsp wicspa@gmail.com
-LastEditTime: 2024-06-05 21:06:30
+LastEditTime: 2024-06-06 16:21:58
 FilePath: /wicspy/wicspy/__init__.py
 Description: 
 
@@ -10,4 +10,4 @@ Copyright (c) 2024 by wicsp, All Rights Reserved.
 '''
 from .bark import *
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
