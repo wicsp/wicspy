40400e7b76e4c1b38fa523aa2350a5c41e5ef77c
wicsp <wicspa@gmail.com>
2024-06-18 19:14:52 +0800

    update

--- build/lib/wicspy/__init__.py
+++ build/lib/wicspy/__init__.py
@@ -2,7 +2,7 @@
 Author: wicsp wicspa@gmail.com
 Date: 2024-06-05 17:29:03
 LastEditors: wicsp wicspa@gmail.com
-LastEditTime: 2024-06-06 16:21:58
+LastEditTime: 2024-06-18 19:09:51
 FilePath: /wicspy/wicspy/__init__.py
 Description: 
 
@@ -10,4 +10,4 @@ Copyright (c) 2024 by wicsp, All Rights Reserved.
 '''
 from .bark import *
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
--- build/lib/wicspy/timer_tool.py
+++ build/lib/wicspy/timer_tool.py
@@ -1,13 +1,11 @@
 '''
 Author: wicsp wicspa@gmail.com
-Date: 2024-06-05 17:29:03
+Date: 2024-06-18 19:12:15
 LastEditors: wicsp wicspa@gmail.com
-LastEditTime: 2024-06-18 19:09:51
-FilePath: /wicspy/wicspy/__init__.py
+LastEditTime: 2024-06-18 19:12:27
+FilePath: /wicspy/wicspy/timer.py
 Description: 
 
 Copyright (c) 2024 by wicsp, All Rights Reserved. 
 '''
-from .bark import *
-
-__version__ = "0.0.7"
+import timer
