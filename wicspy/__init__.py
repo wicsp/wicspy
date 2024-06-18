2fb1ab47a6d1ac31a39a5455b508c374e51c3ebc
wicsp <wicspa@gmail.com>
2024-06-18 19:19:12 +0800

    update

--- wicspy/__init__.py
+++ wicspy/__init__.py
@@ -2,12 +2,13 @@
 Author: wicsp wicspa@gmail.com
 Date: 2024-06-05 17:29:03
 LastEditors: wicsp wicspa@gmail.com
-LastEditTime: 2024-06-18 19:09:51
+LastEditTime: 2024-06-18 19:18:06
 FilePath: /wicspy/wicspy/__init__.py
 Description: 
 
 Copyright (c) 2024 by wicsp, All Rights Reserved. 
 '''
 from .bark import *
+from .timer_tool import *
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
--- wicspy/timer_tool.py
+++ wicspy/timer_tool.py
@@ -2,10 +2,24 @@
 Author: wicsp wicspa@gmail.com
 Date: 2024-06-18 19:12:15
 LastEditors: wicsp wicspa@gmail.com
-LastEditTime: 2024-06-18 19:12:27
-FilePath: /wicspy/wicspy/timer.py
+LastEditTime: 2024-06-18 19:18:19
+FilePath: /wicspy/wicspy/timer_tool.py
 Description: 
 
 Copyright (c) 2024 by wicsp, All Rights Reserved. 
 '''
-import timer
+import time
+import functools
+
+
+def timing(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()  # 记录开始时间
+        result = func(*args, **kwargs)  # 执行函数
+        end_time = time.time()  # 记录结束时间
+        elapsed_time = end_time - start_time  # 计算经过的时间
+        print(
+            f"Function '{func.__name__}' executed in {elapsed_time:.4f} seconds")
+        return result
+    return wrapper
