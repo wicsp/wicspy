1c3ae8bd0bbb1027f495e6824a76b34f4ea7f265
wicsp <wicspa@gmail.com>
2025-03-28 00:52:26 +0800

    Publish version v0.1.8

--- upload_pypi.sh
+++ upload_pypi.sh
@@ -1,31 +1,32 @@
-#!/bin/zsh
-###
- # @Author: wicsp wicspa@gmail.com
- # @Date: 2024-06-05 17:38:35
- # @LastEditors: wicsp wicspa@gmail.com
- # @LastEditTime: 2024-08-26 18:03:00
- # @FilePath: /wicspy/upload_pypi.sh
- # @Description: 
- # 
- # Copyright (c) 2024 by wicsp, All Rights Reserved. 
-### 
-
-# 检查是否提供了 commit message
+# 检查是否提供了 Version
 if [ -z "$1" ]; then
-  echo "Missing Commit Message!"
+  echo "Missing Version! Please provide a version number like 0.1.7"
   exit 1
 fi
 
-COMMIT_MESSAGE=$1
+VERSION=$1
+COMMIT_MESSAGE="Publish version v$VERSION"
 
-# 删除旧的构建文件
-# 创建新的构建文件
-rye build --clean
+# 检测操作系统类型并使用正确的 sed 命令
+echo "Updating version in pyproject.toml..."
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS 版本
+    sed -i '' "s/^version = .*/version = \"$VERSION\"/" pyproject.toml
+else
+    # Linux 版本
+    sed -i "s/^version = .*/version = \"$VERSION\"/" pyproject.toml
+fi
 
-# 上传到 PyPI
-rye publish
 
 # 提交到 git
+echo "Committing to git..."
 git add .
 git commit -m "$COMMIT_MESSAGE"
 git push
+
+# add tag
+echo "Adding tag..."
+git tag -a "v$VERSION" -m "Publish version $VERSION"
+git push origin "v$VERSION"
+
+echo "Done! The version $VERSION has been published to TestPyPI."
